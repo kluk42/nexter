984407d6d8bf620b3b76ff7d93703a48f4e4929b
Nikita Nemykin <nikitanemykin@mail.ru>
2022-11-07 11:27:15 +0700

    Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,26 @@
+import bbcLogo from '../../assets/logo-bbc.png';
+import biLogo from '../../assets/logo-bi.png';
+import forbesLogo from '../../assets/logo-forbes.png';
+import tcLogo from '../../assets/logo-techcrunch.png';
+import logoPath from '../../assets/logo.png';
+import { Button } from '../Button/Button';
+
 import './Header.scss';
 
 export const Header = () => {
-  return <header className="header">Header</header>;
+  return (
+    <header className="header">
+      <img src={logoPath} alt="Nexter logo" className="header__logo" />
+      <h3 className="heading-3 header__heading-first">Your own home</h3>
+      <h1 className="heading-1 header__heading-main">The ultimate personal freedom</h1>
+      <Button title="View our properties" className="header__btn" />
+      <div className="header__seenon-text">Seen on</div>
+      <div className="header__seenon-logos">
+        <img src={bbcLogo} className="header__seenon-logo" alt="BBC logo" />
+        <img src={forbesLogo} className="header__seenon-logo" alt="Forbes logo" />
+        <img src={tcLogo} className="header__seenon-logo" alt="Tech crunch logo" />
+        <img src={biLogo} className="header__seenon-logo" alt="Business insider logo" />
+      </div>
+    </header>
+  );
 };
--- src/components/Realtors/Realtors.tsx
+++ src/components/Realtors/Realtors.tsx
@@ -1,5 +1,30 @@
+import realtorOneImg from '../../assets/realtor-1.jpeg';
+import realtorTwoImg from '../../assets/realtor-2.jpeg';
+import realtorThreeImg from '../../assets/realtor-3.jpeg';
+
 import './Realtors.scss';
 
 export const Realtors = () => {
-  return <div className="realtors">Top 3 realtors</div>;
+  return (
+    <div className="realtors">
+      <h3 className="heading-3">Top 3 realtors</h3>
+      <ul className="realtors__list">
+        <li className="realtors__list-item">
+          <img src={realtorOneImg} alt="Realtor 1" className="realtors__img" />
+          <h4 className="heading-4 heading-4__light">Eric Feinman</h4>
+          <p className="realtors__sold">245 houses sold</p>
+        </li>
+        <li className="realtors__list-item">
+          <img src={realtorTwoImg} alt="Realtor 2" className="realtors__img" />
+          <h4 className="heading-4 heading-4__light">Kim Brown</h4>
+          <p className="realtors__sold">212 houses sold</p>
+        </li>
+        <li className="realtors__list-item">
+          <img src={realtorThreeImg} alt="Realtor 3" className="realtors__img" />
+          <h4 className="heading-4 heading-4__light">Toby Ramsey</h4>
+          <p className="realtors__sold">198 houses sold</p>
+        </li>
+      </ul>
+    </div>
+  );
 };
